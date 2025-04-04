f802217d99aebd78137104394e6b2374117b642c
riteshv2109 <riteshv2109@gmail.com>
2025-04-04 04:28:13 +0000

    casting

---
+++ Casting.java
@@ -0,0 +1,10 @@
+public class Casting{
+    public static void main(String[] args)
+    {
+        int p=100;
+        double fp=p+18.09;
+        System.out.println(fp);
+        /* as 18.0 is double value it can't be stored in int value 
+        to casting is done by writing in starting of value in ( )*/ 
+    }
+}
