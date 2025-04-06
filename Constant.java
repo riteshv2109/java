25257ece6fcf08c60cca155a4ae920cfc96e6357
riteshv2109 <riteshv2109@gmail.com>
2025-04-06 13:30:54 +0000

    constants

---
+++ Constant.java
@@ -0,0 +1,11 @@
+public class Constant
+{
+    public static void main(String[] args)
+    {
+        // constants
+        // final keyword is used to describe constant
+
+        final float PI=3.14F;
+        
+    }
+}
